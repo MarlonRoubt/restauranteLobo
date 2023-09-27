570818be389ced343c3846f8deeb15dcc49cbe1a
Marlon Roubt <roubtmarlon95@gmail.com>
2023-09-27 20:59:57 -0300

    finalizando

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,7 +23,41 @@ function clickMenu() {
     }
 }
 
+// para o nav acima de 768px
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        navDrop.style.display = "block";
+    } else {
+        navDrop.style.display = "none";
+    }
+}
+
 
 function fadeInLeft(element){
 element.style.setProperty("animation", "fadeInLeft 2s");
 }
+
+function scrollToSection(sectionClass) {
+    const section = document.querySelector(sectionClass);
+
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
+
+document.querySelector('#scrollUpButton').addEventListener('click', function() {
+    // Rola para cima para a seção '.sobre'
+    scrollToSection('.sobre');
+    scrollToSection('.nossaCasa');
+    scrollToSection('.reservas');
+
+});
+
+document.querySelector('#scrollDownButton').addEventListener('click', function() {
+    // Rola para baixo para a seção '.nossaCasa'
+    scrollToSection('.nossaCasa');
+    scrollToSection('.reservas');
+    scrollToSection('.formulario');
+});
