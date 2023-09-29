1ec6c3a1864dd08571e2478e53fcc125d9d8aeef
Marlon Roubt <roubtmarlon95@gmail.com>
2023-09-29 01:40:19 -0300

    correcao no menu com js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-
+const navDrop = document.getElementById('navDrop');
 
 function onLoadBlock(){
     navDrop.style.display = 'none';
@@ -11,19 +11,9 @@ function onLoadBlock(){
    })
 
    fadeInLeft(logo);
+   mudouTamanho();
 
 }
-
-
-function clickMenu() {
-    if (navDrop.style.display == 'block') {
-        navDrop.style.display = 'none'; 
-    } else {
-        navDrop.style.display = 'block';
-    }
-}
-
-// para o nav acima de 768px
 
 function mudouTamanho() {
     if (window.innerWidth >= 768) {
@@ -33,31 +23,21 @@ function mudouTamanho() {
     }
 }
 
+function clickMenu() {
+    if (navDrop.style.display == 'block') {
+        navDrop.style.display = 'none'; 
+    } else {
+        navDrop.style.display = 'block';
+    }
+}
+
+mudouTamanho();
+window.addEventListener('resize', mudouTamanho);
+
 
 function fadeInLeft(element){
 element.style.setProperty("animation", "fadeInLeft 2s");
 }
 
-function scrollToSection(sectionClass) {
-    const section = document.querySelector(sectionClass);
-
-    if (section) {
-        section.scrollIntoView({ behavior: 'smooth' });
-    }
-}
 
 
-document.querySelector('#scrollUpButton').addEventListener('click', function() {
-    // Rola para cima para a seção '.sobre'
-    scrollToSection('.sobre');
-    scrollToSection('.nossaCasa');
-    scrollToSection('.reservas');
-
-});
-
-document.querySelector('#scrollDownButton').addEventListener('click', function() {
-    // Rola para baixo para a seção '.nossaCasa'
-    scrollToSection('.nossaCasa');
-    scrollToSection('.reservas');
-    scrollToSection('.formulario');
-});
